901999cc181843e730e21686417db05db7d46f00
Brendan Hansknecht <Brendan.Hansknecht@gmail.com>
2018-01-10 20:32:09 -0500

    Change Ranking Algorithm

--- worker/ranking.py
+++ worker/ranking.py
@@ -4,10 +4,11 @@ from player.aiplayer import AIPlayer
 from othello import Othello
 from time import time
 import numpy as np
+import itertools
 import random
 import choix
-import math
 import glob
+import math
 import os
 
 def start():
@@ -27,43 +28,51 @@ def calc_ranking(config):
     win_matrix = np.zeros((len(players),len(players)))
     game = Othello()
     
-    king_index = len(players)-1
-    king = AIPlayer(0, config.game.simulation_num_per_move, train=False, model=players[king_index], tau=config.game.tau_1)
-    challenger = AIPlayer(0, config.game.simulation_num_per_move, train=False, model=players[0], tau=config.game.tau_1)
+    challenger1 = AIPlayer(0, config.game.simulation_num_per_move, train=False, model=players[-1], tau=config.game.tau_1)
+    challenger2 = AIPlayer(0, config.game.simulation_num_per_move, train=False, model=players[0], tau=config.game.tau_1)
     total_games = (config.game_num_per_model * (len(players)))//2
     played_games = 0
+    finished = False
     start = time()
-    print("Playing king of the hill with %d players and %d games per player" % (len(players), config.game_num_per_model))
+    print("Ranking with %d players and %d games per player" % (len(players), config.game_num_per_model))
     if config.game_num_per_model < len(players):
         print("We suggest that you increase games per player to be greater than players")
-    for i in range(math.ceil(total_games/(len(players)-1))):
-        AIPlayer.clear()
-        king_index = getKingIndex(win_matrix)
-        #first round robin then switch to king
-        if king_index == -1 or i < len(players):
-            king_index = (len(players)-1)-i%len(players)
-            msg = "No King Yet"
+        
+    for i in itertools.count():
+        ranks = getRankings(win_matrix)
+
+        if len(ranks) == 0 or played_games < config.roundrobin_percent*total_games:
+            msg = "No Clear Best Yet"
         else:
-            msg = "King is "+os.path.basename(players[king_index]).split(".")[0]
-        king.load(players[king_index])
-        if config.print_king:
+            msg = "Current Best is "+os.path.basename(players[ranks[-1]]).split(".")[0]    
+        if config.print_ranking_while_running:
             print(msg.ljust(90))
         for j in range(len(players)):
             util.print_progress_bar(played_games, total_games, start=start)
             
-            if j == king_index:
-                continue
+            #either challenge people around you with normal distribution or play least played
+            if len(ranks) != 0 and played_games > config.roundrobin_percent*total_games:
+                my_rank = ranks.index(j)
+                challenger1_index = normalChoiceAroundPlayer(ranks, my_rank)
+            else:
+                challenger1_index = getLeastPlayed(win_matrix, j)
             
-            challenger.load(players[j])
+            if challenger1_index == j:
+                print("error")
+                exit()
+            
+            AIPlayer.clear()
+            challenger1.load(players[challenger1_index])
+            challenger2.load(players[j])
             
             if random.random() < 0.5:
-                king_side = -1
-                p1 = king
-                p2 = challenger
+                challenger1_side = -1
+                p1 = challenger1
+                p2 = challenger2
             else:
-                king_side = 1
-                p1 = challenger
-                p2 = king
+                challenger1_side = 1
+                p1 = challenger2
+                p2 = challenger1
             side = -1
             turn = 1
             while not game.game_over():
@@ -79,45 +88,82 @@ def calc_ranking(config):
                 game.play_move(t[0], t[1], side)
                 side *= -1
                 turn += 1
-            if game.get_winner() == king_side:
-                win_matrix[king_index,j] += 1
-                wtl[king_index,0] += 1
+            if game.get_winner() == challenger1_side:
+                win_matrix[challenger1_index,j] += 1
+                wtl[challenger1_index,0] += 1
                 wtl[j,2] += 1
-            elif game.get_winner() == -1*king_side:
-                win_matrix[j, king_index] += 1
-                wtl[king_index,2] += 1
+            elif game.get_winner() == -1*challenger1_side:
+                win_matrix[j, challenger1_index] += 1
+                wtl[challenger1_index,2] += 1
                 wtl[j,0] += 1
             else:
-                win_matrix[king_index,j] += 0.5
-                win_matrix[j, king_index] += 0.5
-                wtl[king_index,1] += 1
+                win_matrix[challenger1_index,j] += 0.5
+                win_matrix[j, challenger1_index] += 0.5
+                wtl[challenger1_index,1] += 1
                 wtl[j,1] += 1
             game.reset_board()
             played_games += 1
-            if played_games == total_games:
+            if played_games >= total_games:
+                finished = True
                 break
-    util.print_progress_bar(total_games, total_games, start=start)
+        if finished:
+            break
+    util.print_progress_bar(total_games, total_games, start=start)   
+    print("\nWin Matrix(row beat column):")
+    print(win_matrix)
     try:
         params = choix.ilsr_pairwise_dense(win_matrix)
         print("\nRankings:")
         for i, player in enumerate(np.argsort(params)[::-1]):
             print("%d. %s (expected %d) with %0.2f rating and results of %d-%d-%d"% 
-                  (i+1, os.path.basename(players[player]).split(".")[0], len(players)-player, params[player],
+                  (i+1, getPlayerName(players, player), len(players)-player, params[player],
                     wtl[player,0], wtl[player,1], wtl[player,2]))
         print("\n(Rating Diff, Winrate) -> (0.5, 62%), (1, 73%), (2, 88%), (3, 95%), (5, 99%)")
     except Exception:
         print("\n Not Enough data to calculate rankings")
-        print("\nWin Matrix:")
-        print(win_matrix)
         print("\nResults:")
         for player in range(win_matrix.shape[0]):
-            print("%s results of %d-%d-%d"% (os.path.basename(players[player]).split(".")[0], wtl[player,0], wtl[player,1],
+            print("%s results of %d-%d-%d"% (getPlayerName(players, player), wtl[player,0], wtl[player,1],
                                              wtl[player,2]))
     
-def getKingIndex(win_matrix):
+def getRankings(win_matrix):
     try:
         with np.errstate(divide='ignore', invalid='ignore'):
             params = choix.ilsr_pairwise_dense(win_matrix)
-        return np.argsort(params)[-1]
+        return np.argsort(params).tolist()
     except Exception:
-        return -1
+        return []
+    
+def getLeastPlayed(win_matrix, player):
+    min_plays = float("inf")
+    least_played_opponent = -1
+    for i in range(win_matrix.shape[0]):
+        if i == player:
+            continue
+        plays = win_matrix[i,player] + win_matrix[player,i]
+        if least_played_opponent == -1 or plays < min_plays:
+            least_played_opponent = i
+            min_plays = plays
+    return least_played_opponent
+
+def getPlayerName(players, index):
+    return os.path.basename(players[index]).split(".")[0]
+
+def normalChoiceAroundPlayer(ranks, player_rank, reach=None):
+    # if mean is the players location if removed from list
+    mean = player_rank-0.5
+    if mean < 0:
+        mean = 0
+    ranks_without_self = ranks[:player_rank] + ranks[player_rank+1 :]
+    if reach is None:
+        reach = len(ranks_without_self)/2
+    
+    stddev = len(ranks_without_self) * ((reach/len(ranks_without_self))/2)
+    
+    if mean > len(ranks_without_self)-1:
+        mean = len(ranks_without_self)-1
+    
+    while True:
+        index = math.floor(random.gauss(mean, stddev) + 0.5)
+        if 0 <= index < len(ranks_without_self):
+            return ranks_without_self[index]
